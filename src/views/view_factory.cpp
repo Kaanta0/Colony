18f864be43d493c214356af96ecec00e88ceb6cd
Kaanta <107195318+Kaanta0@users.noreply.github.com>
2025-11-13 23:14:29 +0100

    Render Heaven & Earth interface in dashboard

--- src/views/view_factory.cpp
+++ src/views/view_factory.cpp
@@ -1,13 +1,34 @@
 #include "views/view_factory.hpp"
 
 #include "views/simple_text_view.hpp"
+#include "views/heaven_earth_view.hpp"
+
+#include "game/heaven_earth_data.hpp"
 
 namespace colony
 {
 
+ViewFactory::ViewFactory() = default;
+
+std::shared_ptr<game::HeavenEarthCompendium> ViewFactory::EnsureCompendium() const
+{
+    if (!compendium_)
+    {
+        auto instance = std::make_shared<game::HeavenEarthCompendium>();
+        instance->LoadDefault();
+        compendium_ = std::move(instance);
+    }
+    return compendium_;
+}
+
 ViewPtr ViewFactory::CreateSimpleTextView(const std::string& id) const
 {
     return std::make_unique<SimpleTextView>(id);
+}
+
+ViewPtr ViewFactory::CreateHeavenEarthView(const std::string& id) const
+{
+    return std::make_unique<HeavenEarthView>(id, EnsureCompendium());
 }
 
 } // namespace colony
